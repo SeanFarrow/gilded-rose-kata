9874c47ae6bf1ab6cba352a72543d8a62d86c087
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-02-23 14:52:22 +0000

    Rename the GildedRoseTest class in preparation for using the approval tests library.

--- Java/com/gildedrose/GildedRoseApprovalTests.java
+++ Java/com/gildedrose/GildedRoseApprovalTests.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class GildedRoseTest {
+public class GildedRoseApprovalTests {
 
     @Test
     public void testUpdateQuality() {
