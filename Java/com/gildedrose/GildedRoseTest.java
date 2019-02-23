639b787858751edd6bcb3d379b199f9ff6187acf
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-02-23 13:45:26 +0000

    Make the existing test checking the item name is foo pass.

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,11 @@ public class GildedRoseTest {
 
     @Test
     public void testUpdateQuality() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        String expectedItemName ="foo";
+    	Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(expectedItemName, app.items[0].name);
     }
 
 }
