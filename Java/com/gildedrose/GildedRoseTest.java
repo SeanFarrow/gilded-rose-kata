0e9090302172e843d5211bd61d868120b85300da
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-02-23 13:37:46 +0000

    Rename the original test to more correctly indicate what it does.

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
+    public void testUpdateQuality() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
