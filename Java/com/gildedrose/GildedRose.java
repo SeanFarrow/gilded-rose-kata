d4c006ee370f3fbbc722491dc5b2edb7c8d453e7
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-02-23 21:23:31 +0000

    Extract a method to remove all code that evaluates and updates items from the for loop and make the new method the target of any further refactorings.

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -9,54 +9,58 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item updatingItem : items) {
-        	if (!updatingItem.name.equals("Aged Brie")
-                    && !updatingItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (updatingItem.quality > 0) {
-                    if (!updatingItem.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        updatingItem.quality = updatingItem.quality - 1;
-                    }
-                }
-            } else {
-                if (updatingItem.quality < 50) {
-                    updatingItem.quality = updatingItem.quality + 1;
-
-                    if (updatingItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (updatingItem.sellIn < 11) {
-                            if (updatingItem.quality < 50) {
-                                updatingItem.quality = updatingItem.quality + 1;
-                            }
-                        }
-
-                        if (updatingItem.sellIn < 6) {
-                            if (updatingItem.quality < 50) {
-                                updatingItem.quality = updatingItem.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!updatingItem.name.equals("Sulfuras, Hand of Ragnaros")) {
-                updatingItem.sellIn = updatingItem.sellIn - 1;
-            }
-
-            if (updatingItem.sellIn < 0) {
-                if (!updatingItem.name.equals("Aged Brie")) {
-                    if (!updatingItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (updatingItem.quality > 0) {
-                            if (!updatingItem.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                updatingItem.quality = updatingItem.quality - 1;
-                            }
-                        }
-                    } else {
-                        updatingItem.quality = updatingItem.quality - updatingItem.quality;
-                    }
-                } else {
-                    if (updatingItem.quality < 50) {
-                        updatingItem.quality = updatingItem.quality + 1;
-                    }
-                }
-            }
+        	UpdateValuesForAnIndividualItem(updatingItem);
         }
     }
+
+	private void UpdateValuesForAnIndividualItem(Item updatingItem) {
+		if (!updatingItem.name.equals("Aged Brie")
+		        && !updatingItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+		    if (updatingItem.quality > 0) {
+		        if (!updatingItem.name.equals("Sulfuras, Hand of Ragnaros")) {
+		            updatingItem.quality = updatingItem.quality - 1;
+		        }
+		    }
+		} else {
+		    if (updatingItem.quality < 50) {
+		        updatingItem.quality = updatingItem.quality + 1;
+
+		        if (updatingItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+		            if (updatingItem.sellIn < 11) {
+		                if (updatingItem.quality < 50) {
+		                    updatingItem.quality = updatingItem.quality + 1;
+		                }
+		            }
+
+		            if (updatingItem.sellIn < 6) {
+		                if (updatingItem.quality < 50) {
+		                    updatingItem.quality = updatingItem.quality + 1;
+		                }
+		            }
+		        }
+		    }
+		}
+
+		if (!updatingItem.name.equals("Sulfuras, Hand of Ragnaros")) {
+		    updatingItem.sellIn = updatingItem.sellIn - 1;
+		}
+
+		if (updatingItem.sellIn < 0) {
+		    if (!updatingItem.name.equals("Aged Brie")) {
+		        if (!updatingItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+		            if (updatingItem.quality > 0) {
+		                if (!updatingItem.name.equals("Sulfuras, Hand of Ragnaros")) {
+		                    updatingItem.quality = updatingItem.quality - 1;
+		                }
+		            }
+		        } else {
+		            updatingItem.quality = updatingItem.quality - updatingItem.quality;
+		        }
+		    } else {
+		        if (updatingItem.quality < 50) {
+		            updatingItem.quality = updatingItem.quality + 1;
+		        }
+		    }
+		}
+	}
 }
