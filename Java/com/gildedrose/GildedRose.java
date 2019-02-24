7c8d85be3dc783dc855048d413347f066bb41910
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-02-24 01:14:14 +0000

    Lift up backstage passes to it's own function.

--- Java/com/gildedrose/GildedRose.java
+++ Java/com/gildedrose/GildedRose.java
@@ -18,9 +18,38 @@ class GildedRose {
 		{//start aged brie.
 		    updateAgedBrie(updatingItem); 
 		}//end aged brie. 
+		else if (updatingItem.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+			//start backstage.
+    updateBackStage(updatingItem); 
+//end backstage.
+		}
 		else { 
 		foo(updatingItem);
 	}
+	}
+
+	private void updateBackStage(Item updatingItem) {
+		if (updatingItem.quality < 50) {
+		    updatingItem.quality = updatingItem.quality + 1;
+
+		        if (updatingItem.sellIn < 11) {
+		            if (updatingItem.quality < 50) {
+		                updatingItem.quality = updatingItem.quality + 1;
+		            }
+		        }
+
+		        if (updatingItem.sellIn < 6) {
+		            if (updatingItem.quality < 50) {
+		                updatingItem.quality = updatingItem.quality + 1;
+		            }
+		        }
+		    }
+		
+		updatingItem.sellIn = updatingItem.sellIn - 1;
+
+if (updatingItem.sellIn < 0) {
+			    updatingItem.quality = updatingItem.quality - updatingItem.quality;
+		    }
 	}
 
 	private void updateAgedBrie(Item updatingItem) {
