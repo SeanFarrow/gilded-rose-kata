fa7c5560cf6b4dfde527aa30c452cd7d1a1a147c
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-02-25 01:48:10 +0000

    Make the failing unit tests pass for updating conjured items.

--- Java/com/gildedrose/UpdateConjuredItemStrategy.java
+++ Java/com/gildedrose/UpdateConjuredItemStrategy.java
@@ -4,7 +4,20 @@ public class UpdateConjuredItemStrategy  implements UpdateItemStrategy {
 
 	@Override
 	public Item updateItem(Item itemToUpdate) {
-return itemToUpdate;
-	}
+        int newQuality = itemShouldStillBeSold(itemToUpdate) ? itemToUpdate.quality -2 : itemToUpdate.quality -4;
 
+        if(isQualityNegative(newQuality)) {
+            newQuality = 0;
+        }
+
+        return new Item(itemToUpdate.name, itemToUpdate.sellIn - 1, newQuality);
+    }
+
+    private boolean isQualityNegative(int newQuality) {
+        return newQuality < 0;
+    }
+
+    private boolean itemShouldStillBeSold(Item item) {
+        return item.sellIn > -1;
+    }
 }
