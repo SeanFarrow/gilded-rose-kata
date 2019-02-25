41a4fb5c02864a895d99e5c9737162ea4bf3606c
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2019-02-25 14:11:56 +0000

    Add a failing unit test ensuring that the UpdateItemStrategyFactoryImpl returns an UpdateSulfurasItemStrategy when the item name is Sulfuras, Hand of Ragnaros.

--- Java/com/gildedrose/UpdateItemStrategyFactoryImplTest.java
+++ Java/com/gildedrose/UpdateItemStrategyFactoryImplTest.java
@@ -17,5 +17,15 @@ public class UpdateItemStrategyFactoryImplTest {
 	var itemUpdateStrategy =factory.FindUpdateStrategyForItem(itemToUpdate);	
 	assertThat(itemUpdateStrategy, is(instanceOf(UpdateConjuredItemStrategy.class)));
 	}
-
+	
+	@Test
+	public void FindUpdateStrategyForItemReturnsAnUpdateSulfurasItemStrategyWhenTheItemNameIsSulfurasHandOfRagnaros() {
+		String itemName ="Sulfuras, Hand of Ragnaros";
+		int initialSellInValue =-2;
+		int initialQualityValue =1;
+		var itemToUpdate =new Item(itemName, initialSellInValue, initialQualityValue);        
+ var factory =new UpdateItemStrategyFactoryImpl();
+	var itemUpdateStrategy =factory.FindUpdateStrategyForItem(itemToUpdate);	
+	assertThat(itemUpdateStrategy, is(instanceOf(UpdateSulfurasItemStrategy.class)));
+	}
 }
